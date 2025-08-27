6ce6b389b55143e1a93b572831d3090a2d528f21
Greg Ingersoll <gpipuck@gmail.com>
2025-08-27 17:06:05 -0400

    initial commit

---
+++ src/main/java/com/mahony_ingersoll/CatSpring/CatSpringApplication.java
@@ -0,0 +1,13 @@
+package com.mahony_ingersoll.CatSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mahony_ingersoll/CatSpring/CatSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahony_ingersoll.CatSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
